258595d26197a36d37947f5911da72807ddab911
Glitch (quaint-worm) <none>
2018-10-29 10:01:38 +0000

    📯🐴 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,54 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:timestamp", (req,res) => {
+  let time = req.params.timestamp ;
+  console.log(time) ;
+  let ans = {unix: null, utc:null};
+  if(!isNaN(time)) {
+    // its a unix timestamp 
+    ans.unix = time * 1000 ; 
+    ans.utc = new Date(time * 1000).toUTCString() ;
+    res.json(ans) ;
+  } else {
+    // its a date string
+    let temp = Date.parse(time) ;
+    if(isNaN(temp)) {
+      res.json({error: 'Invalid Date'});
+    }
+    else {
+      // valid string
+      ans.unix = temp ; 
+      ans.utc = new Date(temp).toUTCString() ;
+      res.json(ans) ;
+    }
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
